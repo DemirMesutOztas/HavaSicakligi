5d5aa752916def67eb2cc6ba142923990944fca4
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-06 11:11:55 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int heat;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık giriniz: ");
+        heat = input.nextInt();
+
+        if(heat<5)
+        {
+            System.out.println("Kayağa gidebilirsiniz");
+        }
+         if (heat>=5 && heat<=15)
+        {
+            System.out.println("Sinemaya giderbilirsiniz");
+        }
+        if (heat>=15 && heat<=25) {
+            System.out.println("Pikniğe gidebilirsiniz");
+
+        }
+        if (heat>=25) {
+            System.out.println("Yüzmeye gidebilirsiniz");
+
+        }
+
+
     }
 }
